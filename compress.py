a4f0c20658d633a42b204d007f267fba36e87ed1
QuietCuteOwl <217069674+QuietCuteOwl@users.noreply.github.com>
2025-11-30 19:18:50 +0530

    Wrote sudo code for compressing pdf

---
+++ compress.py
@@ -0,0 +1,11 @@
+
+
+# init pdf reader with path to input pdf
+# init writer
+
+# loop through every page in reader and write it to writer
+# loop through every page in writer and compress it
+
+# add meta data to writer from reader
+
+# open output pdf in mode "wb" and write to it from writer
