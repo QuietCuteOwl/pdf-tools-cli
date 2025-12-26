758c7737c40ab9dc0ba35c95ec9224561d13b268
QuietCuteOwl <217069674+QuietCuteOwl@users.noreply.github.com>
2025-12-26 20:17:23 +0530

    Rewrote all functions in operations.py to use pikepdf instead of pypdf.

--- cli.py
+++ cli.py
@@ -1,4 +1,5 @@
 import click
+from pathlib import Path
 from operations import Operations as ops, ensure_dir
 
 @click.group()
@@ -21,6 +22,57 @@ def compress(input_pdf, output_dir):
     ensure_dir(output_dir)
     
     ops.compress(str(input_pdf), str(output_dir))
+
+
+@main.command()
+@click.argument('input_list', nargs=-1, type=click.Path(exists=True, path_type=Path))
+@click.option('-o', '--output', 'output_dir', required=False, help="Output dirertory")
+def merge(input_list, output_dir):
+    ensure_dir(output_dir)
+    ops.merge(input_list, str(output_dir))
+
+
+@main.command()
+@click.argument('input_pdf')
+@click.option('-o', '--output', 'output_dir', required=False, help="Output Directory")
+def split(input_pdf, output_dir):
+    ensure_dir(output_dir)
+    ops.split(str(input_pdf), str(output_dir))
+
+
+@main.command()
+@click.argument('input_pdf')
+@click.option('-o', '--output', 'output_dir', required=False, help="Output Directory")
+def extract(input_pdf, output_dir):
+    ensure_dir(output_dir)
+    print(ops.extract_text(str(input_pdf), str(output_dir)))
+
+
+@main.command()
+@click.argument('source_pdf', type=click.Path(exists=True))
+@click.argument('page_num', type=int)
+@click.argument('target_pdf', type=click.Path(exists=True))
+@click.option('-o', '--output', 'output_dir', required=False, default='.', help="Output Directory")
+def add(source_pdf, page_num: int, target_pdf, output_dir):
+
+    ops.add_page(str(source_pdf), page_num, str(target_pdf), str(output_dir))
+
+
+@main.command()
+@click.argument('input_pdf', type=click.Path(exists=True))
+@click.argument('page_num', type=int)
+@click.option('-o', '--output', 'output_dir', required=False, default='.', help="Output Directory")
+def remove(input_pdf, page_num, output_dir):
+
+    ops.remove_page(str(input_pdf), page_num, str(output_dir))
+
+
+@main.command()
+@click.argument('input_pdf', type=click.Path(exists=True))
+@click.argument('watermark_pdf', type=click.Path(exists=True))
+@click.option('-o', '--output', 'output_dir', required=False, default='.', help="Output Directory")
+def watermark(input_pdf, watermark_pdf, output_dir):
+    ops.watermark(str(input_pdf), str(watermark_pdf), str(output_dir))
 
 
 if __name__ == "__main__":
--- operations.py
+++ operations.py
@@ -5,15 +5,8 @@ from typing import List
 class Operations:
     def compress(input_pdf: str, output_path: str):
 
-        name, ext = os.path.splitext(os.path.basename(input_pdf))
-        name = f"{name}_compressed"
-        # change the destination_path to ../filename(n).pdf
-        n = 1
-        destination = os.path.join(output_path, (name + ext))
-        while os.path.exists(destination):
-            # name = name(n) where n is the number of files with the same name - 1
-            destination = os.path.join(output_path, ((name + '(' + str(n) + ')' ) + ext))
-            n += 1
+        name, _ = os.path.splitext(os.path.basename(input_pdf))
+        destination = make_output_pdf(f"{name}_compressed", output_path)
 
         try:
             with pikepdf.open(input_pdf) as pdf:
@@ -28,92 +21,88 @@ class Operations:
 
         return
 
-    # def merge(pdf_paths: List[str], output_path: str):
-
-    #     writer = PdfWriter()
-
-    #     for pdf in pdf_paths:
-    #         writer.append(pdf)
+    def merge(pdf_list: List[str], output_path: str):
         
-    #     with open(output_path, "wb") as f:
-    #         writer.write(f)
-
-
-    # def split(input_path: str, output_dir: str):
-
-    #     reader = PdfReader(input_path)
-
-    #     for i, page in enumerate(reader.pages):
-    #         writer = PdfWriter()
-    #         writer.add_page(page)
-            
-    #         output_filename = os.path.join(output_dir, f"page_{i+1}.pdf")
-    #         with open(output_filename, "wb") as f:
-    #             writer.write(f)
-
-
-    # def extract_text(input_path: str) -> str:
-    #     reader = PdfReader(input_path)
-
-    #     text = ""
-
-    #     for page in reader.pages:
-    #         text += page.extract_text() + "\n"
-            
-    #     return text
-    
-
-    # def add_page(source_pdf: str, page_num: int, target_pdf: str, output_path: str):
-    #     # page_num is 1-based index for user friendliness
-
-    #     reader_source = PdfReader(source_pdf)
-    #     reader_target = PdfReader(target_pdf)
-
-    #     writer = PdfWriter()
-
-    #     for page in reader_target.pages:
-    #         writer.add_page(page)
-
-    #     if 0 < page_num <= len(reader_source.pages):
-    #         writer.add_page(reader_source.pages[page_num - 1])
-    #     else:
-    #         raise ValueError(f"Page number {page_num} is out of range for source PDF")
-
-    #     with open(output_path, "wb") as f:
-    #         writer.write(f)
-
-
-    # def remove_page(input_path: str, page_num: int, output_path: str):
-    #     # page_num is 1-based index
-
-    #     reader = PdfReader(input_path)
-    #     writer = PdfWriter()
-
-    #     for i, page in enumerate(reader.pages):
-    #         if i != (page_num - 1):
-    #             writer.add_page(page)
-
-    #     with open(output_path, "wb") as f:
-    #         writer.write(f)
-
-
-    # def watermark(input_path: str, watermark_path: str, output_path: str):
-
-    #     reader_input = PdfReader(input_path)
-    #     reader_watermark = PdfReader(watermark_path)
-
-    #     watermark_page = reader_watermark.pages[0]
-
-    #     writer = PdfWriter()
-
-    #     for page in reader_input.pages:
-    #         page.merge_page(watermark_page)
-    #         writer.add_page(page)
+        destination = make_output_pdf(f"merged", output_path)
+        pdf_merged = pikepdf.new()
+        for pdf_file in pdf_list:
+            with pikepdf.open(pdf_file) as src:
+                pdf_merged.pages.extend(src.pages)
         
-    #     with open(output_path, "wb") as f:
-    #         writer.write(f)
+        try:
+            pdf_merged.save(destination)
+        except Exception as e:
+            raise(e)
+
+    def split(input_pdf: str, output_dir: str):
+        
+        with pikepdf.open(input_pdf) as src:
+            for i, page in enumerate(src.pages):
+                tmp_pdf = pikepdf.new()
+                tmp_pdf.pages.append(page)
+                destination = make_output_pdf(f"{i + 1}_split", output_dir)
+                tmp_pdf.save(destination)
+
+
+    def extract_text(input_pdf, output_dir):
+        import fitz
+        
+        src = fitz.open(input_pdf)
+        text = ""
+
+        for page in src:
+            text += page.get_text()
+
+        return text
+
+    def add_page(source_pdf: str, page_num: int, target_pdf: str, output_dir: str):
+        tmp_pdf = pikepdf.new()
+        
+        with pikepdf.open(target_pdf) as target:
+            with pikepdf.open(source_pdf) as src:
+                tmp_pdf.pages.extend(target.pages)
+                if not page_num > len(src.pages):
+                    tmp_pdf.pages.append(src.pages[page_num - 1])
+                    name, _ = os.path.splitext(os.path.basename(target_pdf))
+                    tmp_pdf.save(make_output_pdf(f"{name}_add", output_dir))
+                else:
+                    raise IndexError('Page number not in pdf')
+
+        
+    def remove_page(input_pdf: str, page_num: int, output_dir: str):
+        tmp_pdf = pikepdf.new()
+
+        with pikepdf.open(input_pdf) as src:
+            for i, page in enumerate(src.pages):
+                if i != page_num - 1:
+                    tmp_pdf.pages.append(page)
+            name, _ = os.path.splitext(os.path.basename(input_pdf))
+            tmp_pdf.save(make_output_pdf(f"{name}_removed", output_dir))
+
+    def watermark(input_pdf: str, watermark_pdf: str, output_path: str):
+
+        with pikepdf.open(input_pdf) as src, pikepdf.open(watermark_pdf) as wm:
+            wm_page = wm.pages[0]
+
+            for page in src.pages:
+                page.overlay(wm_page, top=True)
+
+            name, _ = os.path.splitext(os.path.basename(input_pdf))
+            src.save(make_output_pdf(f"{name}_watermarked", output_dir))
 
 def ensure_dir(path):
     if not os.path.isdir(path):
         os.makedirs(path)
     return
+
+
+def make_output_pdf(name, output_path):
+    # change the destination_path to ../filename(n).pdf
+    n = 1
+    destination = os.path.join(output_path, (name + '.pdf'))
+    while os.path.exists(destination):
+        # name = name(n) where n is the number of files with the same name - 1
+        destination = os.path.join(output_path, ((name + '(' + str(n) + ')' ) + '.pdf'))
+        n += 1
+
+    return destination
